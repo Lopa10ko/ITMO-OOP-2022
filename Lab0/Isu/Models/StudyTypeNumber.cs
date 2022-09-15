2d0e07b33d8f9a73e4082dbbfd9d2598625d067c
George Lopatenko <lopa10ko239@mail.ru>
2022-09-16 01:16:00 +0300

    feat: add StudyTypeNumber class

---
+++ Lab0/Isu/Models/StudyTypeNumber.cs
@@ -0,0 +1,37 @@
+﻿using Isu.Tools;
+
+namespace Isu.Models;
+
+public class StudyTypeNumber : IEquatable<StudyTypeNumber>
+{
+    private const int MinStudyType = 2;
+    private const int MaxStudyType = 5;
+    private readonly int _studyTypeNumber;
+
+    public StudyTypeNumber(int studyTypeNumber)
+    {
+        ValidateStudyType(studyTypeNumber);
+        _studyTypeNumber = studyTypeNumber;
+    }
+
+    public bool Equals(StudyTypeNumber? other)
+    {
+        return other is not null && _studyTypeNumber == other._studyTypeNumber;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as StudyTypeNumber);
+    }
+
+    public override int GetHashCode()
+        => _studyTypeNumber;
+
+    private void ValidateStudyType(int studyTypeNumber)
+    {
+        if (studyTypeNumber is < MinStudyType or > MaxStudyType)
+        {
+            throw new GroupNameException($"StudyType {studyTypeNumber} is not valid: out of range");
+        }
+    }
+}
