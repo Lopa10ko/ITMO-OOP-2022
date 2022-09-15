90d1dd2227c2fd725ac7ebd1e5fd293c6d6b8d6b
George Lopatenko <lopa10ko239@mail.ru>
2022-09-16 01:35:55 +0300

    feat: add FacultyLetter class

---
+++ Lab0/Isu/Models/FacultyLetter.cs
@@ -0,0 +1,35 @@
+﻿using Isu.Tools;
+
+namespace Isu.Models;
+
+public class FacultyLetter : IEquatable<FacultyLetter>
+{
+    private readonly char _facultyLetter;
+
+    public FacultyLetter(char facultyLetter)
+    {
+        ValidateFacultyLetter(facultyLetter);
+        _facultyLetter = facultyLetter;
+    }
+
+    public bool Equals(FacultyLetter? other)
+    {
+        return other is not null && _facultyLetter == other._facultyLetter;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as FacultyLetter);
+    }
+
+    public override int GetHashCode()
+        => _facultyLetter.GetHashCode();
+
+    private void ValidateFacultyLetter(char facultyLetter)
+    {
+        if (!char.IsUpper(facultyLetter))
+        {
+            throw new GroupNameException($"Faculty letter in GroupName is invalid: {facultyLetter}");
+        }
+    }
+}
