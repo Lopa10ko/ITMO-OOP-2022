2185b3b4a218fcd593fce75311e34bdfa7d25c90
lopa10ko <lopa10ko239@mail.ru>
2022-09-25 22:29:47 +0300

    feat: implement (ProductCard, ProductQuantity)

---
+++ Lab1/Shops/Models/ProductQuantity.cs
@@ -0,0 +1,28 @@
+﻿using Shops.Entities;
+using Shops.Tools;
+
+namespace Shops.Models;
+
+public record ProductQuantity
+{
+    private const int MinProductQuantity = 0;
+    private int _quantity;
+    public ProductQuantity(Product product, int quantity)
+    {
+        Product = product;
+        Quantity = quantity;
+    }
+
+    public int Quantity
+    {
+        get => _quantity;
+        private set
+        {
+            if (value < MinProductQuantity)
+                throw ValueException.InvalidQuantityValue(this, value);
+            _quantity = value;
+        }
+    }
+
+    public Product Product { get; }
+}
