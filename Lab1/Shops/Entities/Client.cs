ccdff86cbe0e0c93626c27b834d0984ed8ae8d97
lopa10ko <lopa10ko239@mail.ru>
2022-09-25 22:30:54 +0300

    feat: implement (Client, Shop)

--- Lab1/Shops/Entities/Client.cs
+++ Lab1/Shops/Entities/Client.cs
@@ -1,22 +1,37 @@
-﻿namespace Shops.Entities;
+﻿using Shops.Models;
+using Shops.Tools;
+
+namespace Shops.Entities;
 
 public class Client : IEquatable<Client>
 {
-    private List<Product> _cart;
+    private const decimal MinMoneyAmount = 0;
+    private List<Product> _productHistory;
+    private decimal _money;
 
-    public Client(int idClient, string clientName, decimal moneyAmount)
+    public Client(Guid idClient, string clientName, decimal moneyAmount)
     {
         Name = clientName;
         MoneyBank = moneyAmount;
         Id = idClient;
-        _cart = new List<Product>();
+        _productHistory = new List<Product>();
     }
 
     public string Name { get; }
-    public decimal MoneyBank { get; }
-    public int Id { get; }
 
-    public IReadOnlyList<Product> GetProducts => _cart;
+    public decimal MoneyBank
+    {
+        get => _money;
+        private set
+        {
+            ValidateMoneyAmount(value);
+            _money = value;
+        }
+    }
+
+    public Guid Id { get; }
+
+    public IReadOnlyList<Product> GetProductHistory() => _productHistory;
 
     public bool Equals(Client? other)
         => other is not null && MoneyBank.Equals(other.MoneyBank) && Id.Equals(other.Id);
@@ -25,5 +40,21 @@ public class Client : IEquatable<Client>
         => Equals(obj as Client);
 
     public override int GetHashCode()
-        => HashCode.Combine(MoneyBank, Id);
+        => Id.GetHashCode();
+
+    internal void TryGetClientMoney(decimal totalPrice)
+    {
+        if (totalPrice > MoneyBank)
+            throw MoneyBankException.InvalidMoneyBankState(this);
+        MoneyBank -= totalPrice;
+    }
+
+    internal void AddProductsToProductHistory(List<Product> products)
+        => _productHistory.AddRange(products);
+
+    private static void ValidateMoneyAmount(decimal moneyAmount)
+    {
+        if (moneyAmount < MinMoneyAmount)
+            throw MoneyBankException.InvalidMoneyBankValue(moneyAmount);
+    }
 }
--- Lab1/Shops/Entities/Shop.cs
+++ Lab1/Shops/Entities/Shop.cs
@@ -1,5 +1,91 @@
-﻿namespace Shops.Entities;
+﻿using Shops.Models;
+using Shops.Tools;
 
-public class Shop
+namespace Shops.Entities;
+
+public class Shop : IEquatable<Shop>
 {
+    private List<ProductCard> _products;
+    public Shop(Guid id, string shopName, Address address)
+    {
+        Id = id;
+        Name = shopName;
+        Address = address;
+        _products = new List<ProductCard>();
+    }
+
+    public string Name { get; }
+    public Guid Id { get; }
+    public Address Address { get; }
+
+    public bool Equals(Shop? other)
+        => other is not null && Id.Equals(other.Id);
+
+    public override bool Equals(object? obj)
+        => Equals(obj as Shop);
+
+    public override int GetHashCode()
+        => HashCode.Combine(Id, Address);
+
+    public void CheckProductPresence(List<ProductQuantity> productList)
+    {
+        if (productList
+            .Select(pr => pr.Product)
+            .Any(product => !IsProductInShop(product)))
+        {
+            throw ShopLogicException.InvalidProductQuantity(this);
+        }
+
+        if (productList
+            .Any(pq => _products.Where(pc => pq.Product.Equals(pc.Product)).Any(pc => pq.Quantity > pc.Quantity)))
+        {
+            throw ShopLogicException.InvalidProductQuantity(this);
+        }
+    }
+
+    internal void AddProductCard(Product product, decimal price, int quantity)
+    {
+        if (IsProductInShop(product))
+        {
+            _products
+                .Single(pc => pc.Product.Equals(product))
+                .SetNewQuantity(quantity);
+        }
+        else
+        {
+            _products.Add(new ProductCard(product, price, quantity));
+        }
+    }
+
+    internal IReadOnlyList<ProductCard> GetProductCardList() => _products;
+
+    internal decimal GetTotalPrice(List<ProductQuantity> productList)
+    {
+        return productList
+            .Sum(pq => _products.Where(pc => pc.Product.Equals(pq.Product))
+                .Sum(pc => pc.Price * pq.Quantity));
+    }
+
+    internal List<Product> Purchase(List<ProductQuantity> productList)
+    {
+        foreach (ProductQuantity pq in productList)
+        {
+            foreach (ProductCard pc in _products.Where(pc => pc.Product.Equals(pq.Product)))
+            {
+                pc.SetNewQuantity(-pq.Quantity);
+            }
+        }
+
+        return productList.Select(pq => pq.Product).ToList();
+    }
+
+    internal void SetNewProductPrice(Product product, decimal newPrice)
+    {
+        if (!IsProductInShop(product))
+            throw ShopLogicException.ProductNotInShop(this, product);
+        _products.Single(pc => pc.Product.Equals(product)).SetNewPrice(newPrice);
+    }
+
+    private bool IsProductInShop(Product product)
+        => _products.Any(pc => pc.Product.Equals(product));
 }
