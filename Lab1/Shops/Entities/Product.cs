1df48ee51ae00d78932297a682973839576a2434
lopa10ko <lopa10ko239@mail.ru>
2022-09-25 22:18:30 +0300

    feat: add (Product)

--- Lab1/Shops/Entities/Product.cs
+++ Lab1/Shops/Entities/Product.cs
@@ -1,6 +1,36 @@
-﻿namespace Shops.Entities;
+﻿using System.Text.RegularExpressions;
+using Shops.Tools;
 
-public class Product
+namespace Shops.Entities;
+
+public class Product : IEquatable<Product>
 {
-    
+    private const string NamePattern = @"^[a-z\-.,0-9\s]*$";
+    public Product(Guid id, string name)
+    {
+        ValidateName(name);
+        Name = name;
+        Id = id;
+    }
+
+    public string Name { get; }
+
+    public Guid Id { get; }
+
+    public bool Equals(Product? other)
+        => other is not null && Id.Equals(other.Id);
+
+    public override bool Equals(object? obj)
+        => Equals(obj as Product);
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Id);
+    }
+
+    private static void ValidateName(string name)
+    {
+        if (!Regex.IsMatch(name, NamePattern, RegexOptions.IgnoreCase))
+            throw NamingException.Create(name);
+    }
 }
