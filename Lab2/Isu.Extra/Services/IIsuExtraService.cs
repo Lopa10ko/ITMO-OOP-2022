ddb402af70f2ef844f4224e7f853d3cf51137fcb
lopa10ko <lopa10ko239@mail.ru>
2022-10-11 22:21:23 +0300

    feat: map out IIsuExtraService

---
+++ Lab2/Isu.Extra/Services/IIsuExtraService.cs
@@ -0,0 +1,19 @@
+﻿using Isu.Entities;
+using Isu.Extra.Entities;
+using Isu.Extra.Models;
+using Isu.Models;
+
+namespace Isu.Extra.Services;
+
+public interface IIsuExtraService
+{
+    OgnpGroup AddOgnpGroup(OgnpGroupName name);
+    void AddOgnpGroupSchedule(OgnpGroup ognpGroup, Schedule schedule);
+    void AddGroupSchedule(Group group, Schedule schedule);
+    void AddStudent(OgnpGroup ognpGroup, Student student);
+    void RemoveStudent(OgnpGroup ognpGroup, Student student);
+    IReadOnlyList<Student> GetStudents(FacultyLetter facultyLetter);
+    IReadOnlyList<OgnpGroup> GetOgnpGroups(FacultyLetter facultyLetter);
+    IReadOnlyList<Student> FindStudents(OgnpGroupName ognpGroupName);
+    IReadOnlyList<Student> FindNotSignedUpStudents(Group group);
+}
