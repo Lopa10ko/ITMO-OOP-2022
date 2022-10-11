62264627c88de802f8b5fb0c6eefc2d00a4145bc
lopa10ko <lopa10ko239@mail.ru>
2022-10-11 22:21:40 +0300

    feat: map out Schedule interface

---
+++ Lab2/Isu.Extra/Services/IScheduleService.cs
@@ -0,0 +1,13 @@
+﻿using Isu.Entities;
+using Isu.Extra.Entities;
+
+namespace Isu.Extra.Services;
+
+public interface IScheduleService
+{
+    void AddSchedule(Group group, Schedule schedule);
+    void AddSchedule(OgnpGroup ognpGroup, Schedule schedule);
+
+    IReadOnlyList<KeyValuePair<Group, Schedule>> GetGroupSchedules();
+    IReadOnlyList<KeyValuePair<OgnpGroup, Schedule>> GetOgnpGroupSchedules();
+}
