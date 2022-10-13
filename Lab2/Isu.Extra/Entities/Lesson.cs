d6e1b28030d60e47e73dc6d7bcc97cb4910372e5
lopa10ko <lopa10ko239@mail.ru>
2022-10-13 22:44:55 +0300

    fix: simplify validation policy within Lesson constructor

--- Lab2/Isu.Extra/Entities/Lesson.cs
+++ Lab2/Isu.Extra/Entities/Lesson.cs
@@ -4,20 +4,17 @@ namespace Isu.Extra.Entities;
 
 public record Lesson
 {
-    public Lesson(string name, string professor, int room, DayOfWeek dayOfWeek, TimeOnly startTime, TimeOnly endTime)
+    public Lesson(string name, Professor professor, int room, DayOfWeek dayOfWeek, TimeOnly startTime, TimeOnly endTime)
     {
         Name = name;
         Professor = professor;
         DayOfWeek = dayOfWeek;
-        ValidateRoom(room);
-        Room = room;
-        ValidateTime(startTime, endTime);
-        StartTime = startTime;
-        EndTime = endTime;
+        Room = ValidateRoom(room);
+        (StartTime, EndTime) = ValidateTime(startTime, endTime);
     }
 
     public string Name { get; }
-    public string Professor { get; }
+    public Professor Professor { get; }
     public int Room { get; }
     public DayOfWeek DayOfWeek { get; }
     public TimeOnly StartTime { get; }
@@ -27,15 +24,17 @@ public record Lesson
         => DayOfWeek.Equals(other.DayOfWeek)
            && !(EndTime < other.StartTime || other.EndTime < StartTime);
 
-    private static void ValidateTime(TimeOnly startTime, TimeOnly endTime)
+    private static (TimeOnly, TimeOnly) ValidateTime(TimeOnly startTime, TimeOnly endTime)
     {
         if (startTime > endTime)
             throw LessonException.InvalidTime(startTime, endTime);
+        return (startTime, endTime);
     }
 
-    private static void ValidateRoom(int room)
+    private static int ValidateRoom(int room)
     {
         if (room < 0)
             throw LessonException.InvalidRoom(room);
+        return room;
     }
 }
