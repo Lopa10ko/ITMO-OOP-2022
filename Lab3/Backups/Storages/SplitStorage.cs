3586c204c0c15d5dcc67963e8d8c59d4bbb711f8
lopa10ko <lopa10ko239@mail.ru>
2022-11-10 00:22:40 +0300

    feat: add (SplitStorageAggregator, ZipArchivedStorage)

---
+++ Lab3/Backups/Storages/SplitStorage.cs
@@ -0,0 +1,16 @@
+﻿using Backups.RepositoryItems;
+
+namespace Backups.Storages;
+
+public class SplitStorage : IStorage
+{
+    private readonly IEnumerable<IStorage> _archivedStorages;
+
+    public SplitStorage(IEnumerable<IStorage> archivedStorages)
+    {
+        _archivedStorages = archivedStorages;
+    }
+
+    public IEnumerable<IRepositoryItem> GetRepositoryItems()
+        => _archivedStorages.SelectMany(storage => storage.GetRepositoryItems());
+}
---
+++ Lab3/Backups/Storages/ZipArchivedStorage.cs
@@ -0,0 +1,25 @@
+﻿using Backups.Repositories;
+using Backups.RepositoryItems;
+using Backups.ZipArchivedItems;
+
+namespace Backups.Storages;
+
+public class ZipArchivedStorage : IStorage
+{
+    public ZipArchivedStorage(string relativePath, IRepository repository, IEnumerable<IZipArchivedItem> items)
+    {
+        Repository = repository;
+        Items = items;
+        RelativeId = relativePath;
+    }
+
+    public IEnumerable<IZipArchivedItem> Items { get; }
+    public string RelativeId { get; }
+    public IRepository Repository { get; }
+
+    public IEnumerable<IRepositoryItem> GetRepositoryItems()
+    {
+        /*=> Items.Select(zi => zi.GetRepositoryItem(zi.Entry)).ToList();*/
+        throw new NotImplementedException();
+    }
+}
