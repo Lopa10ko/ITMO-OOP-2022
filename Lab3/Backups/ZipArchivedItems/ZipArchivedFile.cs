f1dd09a2cc268571442f5ed4a1cdf21df3b94c56
lopa10ko <lopa10ko239@mail.ru>
2022-11-10 00:20:29 +0300

    feat: add major ArchivedObjects cluster

---
+++ Lab3/Backups/ZipArchivedItems/ZipArchivedFile.cs
@@ -0,0 +1,21 @@
+﻿using System.IO.Compression;
+using Backups.RepositoryItems;
+
+namespace Backups.ZipArchivedItems;
+
+public class ZipArchivedFile : IZipArchivedItem
+{
+    public ZipArchivedFile(string name, ZipArchiveEntry fileEntry)
+    {
+        Name = name;
+        Entry = fileEntry;
+    }
+
+    public ZipArchiveEntry Entry { get; }
+    public string Name { get; }
+
+    public IRepositoryItem GetRepositoryItem(ZipArchiveEntry zipArchiveEntry)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Lab3/Backups/ZipArchivedItems/ZipArchivedNode.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Compression;
+using Backups.RepositoryItems;
+
+namespace Backups.ZipArchivedItems;
+
+public class ZipArchivedNode : IZipArchivedItem
+{
+    private readonly List<IZipArchivedItem> _items;
+
+    public ZipArchivedNode(string name, List<IZipArchivedItem> items)
+    {
+        Name = name;
+        _items = items;
+    }
+
+    public string Name { get; }
+
+    public IRepositoryItem GetRepositoryItem(ZipArchiveEntry zipArchiveEntry)
+    {
+        throw new NotImplementedException();
+    }
+}
