c85b79c1d6d6d16898839364675c10a261964549
lopa10ko <lopa10ko239@mail.ru>
2022-11-11 17:39:06 +0300

    feat: implement InMemoryRepository with Zio

---
+++ Lab3/Backups.Test/Repositories/InMemoryRepository.cs
@@ -0,0 +1,59 @@
+﻿using Backups.Models;
+using Backups.Repositories;
+using Backups.RepositoryItems;
+using Backups.Tools;
+using Microsoft.VisualBasic;
+using Zio;
+using Zio.FileSystems;
+
+namespace Backups.Test.Repositories;
+
+public class InMemoryRepository : IRepository
+{
+    public InMemoryRepository(string sourcePath, IFileSystem fileSystem)
+    {
+        Validate(sourcePath);
+        Source = sourcePath;
+        FileSystem = fileSystem;
+    }
+
+    private string Source { get; }
+    private IFileSystem FileSystem { get; }
+
+    public Stream OpenStream(string archiveName)
+         => FileSystem.OpenFile($"{Source}\\{archiveName}", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+    public IRepositoryItem GenerateRepositoryItem(IBackupItem backupItem)
+    {
+        string backupItemRelativeId = backupItem.GetPath();
+        string backupItemFullPath = $"{Source}{backupItemRelativeId}";
+        if (FileSystem.FileExists(backupItemFullPath))
+            return new FileSystemLeaf(backupItemRelativeId, backupItemFullPath, () => FileSystem.OpenFile(backupItemFullPath, FileMode.Open, FileAccess.Read));
+        if (FileSystem.DirectoryExists(backupItemFullPath))
+            return new FileSystemNode(backupItemRelativeId, backupItemFullPath, () => GetItemsList(backupItemFullPath));
+        throw FileSystemRepositoryException.InvalidBackupItem(backupItem);
+    }
+
+    public string GetSource() => Source;
+    private static string GetDirName(string fullPath)
+        => fullPath.Split(UPath.DirectorySeparator).Last();
+    private static void Validate(string sourcePath)
+    {
+        if (!sourcePath.EndsWith(UPath.DirectorySeparator))
+            throw FileSystemRepositoryException.IncorrectDirectoryStringFormat(sourcePath);
+    }
+
+    private IReadOnlyCollection<IRepositoryItem> GetItemsList(string directoryId)
+        => new List<IRepositoryItem>(FileSystem.EnumerateFiles(directoryId, "*", SearchOption.TopDirectoryOnly)
+                .Select(upath => GetDirName(upath.FullName))
+                .Select(fn => new FileSystemLeaf(
+                    fn,
+                    $"{directoryId}{UPath.DirectorySeparator}{fn}",
+                    () => FileSystem.OpenFile($"{directoryId}{UPath.DirectorySeparator}{fn}", FileMode.Open, FileAccess.Read))))
+            .Concat(new List<IRepositoryItem>(FileSystem.EnumerateDirectories(directoryId, "*", SearchOption.TopDirectoryOnly)
+                .Select(upath => upath.FullName)
+                .Select(dn => new FileSystemNode(
+                    GetDirName(dn),
+                    $"{directoryId}{Path.DirectorySeparatorChar}{GetDirName(dn)}",
+                    () => GetItemsList($"{directoryId}{Path.DirectorySeparatorChar}{GetDirName(dn)}"))))).ToList();
+}
