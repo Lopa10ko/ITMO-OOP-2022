d9cb4382424b9fcfe932a55958d3e1a49aa6e3b2
lopa10ko <lopa10ko239@mail.ru>
2022-11-23 19:05:22 +0300

    feat: composite DepositRules via stateful builder

---
+++ Lab4/Banks/Entities/DepositRules.cs
@@ -0,0 +1,41 @@
+﻿namespace Banks.Entities;
+
+public class DepositRules
+{
+    private List<DepositRule> _depositRules;
+
+    private DepositRules(List<DepositRule> rules)
+    {
+        MaximumPercent = rules.Max(rule => rule.Percent);
+        _depositRules = rules.OrderBy(rule => rule.CriticalValue).ToList();
+    }
+
+    public static DepositRulesBuilder Builder => new DepositRulesBuilder();
+    public IReadOnlyList<DepositRule> Rules => _depositRules.AsReadOnly();
+    public decimal MaximumPercent { get; }
+
+    public class DepositRulesBuilder
+    {
+        private readonly List<DepositRule> _rules = new List<DepositRule>();
+
+        public DepositRules Build()
+        {
+            return new DepositRules(_rules);
+        }
+
+        public DepositRulesBuilder AddDepositRule(DepositRule rule)
+        {
+            if (_rules.Contains(rule))
+                return this;
+            bool overlapping = _rules.Any(r => r.IsOverlapping(rule));
+            if (overlapping)
+            {
+                DepositRule overlappingRule = _rules.Single(r => r.IsOverlapping(rule));
+                rule = rule.GetMaximumPercent(overlappingRule);
+            }
+
+            _rules.Add(rule);
+            return this;
+        }
+    }
+}
