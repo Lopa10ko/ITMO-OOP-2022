8267710dca83ecba0e43b61560de2d3edcb2fc80
lopa10ko <lopa10ko239@mail.ru>
2022-11-23 19:03:30 +0300

    feat: add basic entity interfaces

---
+++ Lab4/Banks/Models/IClient.cs
@@ -0,0 +1,14 @@
+﻿using Banks.Accounts;
+
+namespace Banks.Models;
+
+public interface IClient
+{
+    bool IsVerified { get; }
+    Guid Id { get; }
+    string Name { get; }
+    string LastName { get; }
+    string Address { get; }
+    string Passport { get; }
+    void AddAccount(IAccount account);
+}
---
+++ Lab4/Banks/Services/IVerificationdState.cs
@@ -0,0 +1,14 @@
+﻿namespace Banks.Services;
+
+public interface IVerificationState
+{
+    void UpdateVerificationState();
+}
+
+public class VerificationState : IVerificationState
+{
+    public void UpdateVerificationState()
+    {
+        throw new NotImplementedException();
+    }
+}
