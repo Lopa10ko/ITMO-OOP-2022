f0dc892c6d9c5b9e4d2f4fb8ed64d675147af556
lopa10ko <lopa10ko239@mail.ru>
2022-11-23 19:02:51 +0300

    feat: add IAccount

---
+++ Lab4/Banks/Accounts/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using System.Transactions;
+
+namespace Banks.Accounts;
+
+public interface IAccount
+{
+    void IncreaseMoneyValue(decimal value);
+    void DecreaseMoneyValue(decimal value);
+    void RemitTo(decimal value, IAccount account);
+    int GetCommissionPeriod();
+    void RemoveWithdrawLimit();
+    void EvaluateCommission();
+    void AccrueCommission();
+    IReadOnlyList<Transaction> GetTransactions();
+}
