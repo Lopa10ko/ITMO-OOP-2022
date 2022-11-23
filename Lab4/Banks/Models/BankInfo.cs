d9d2626153614f78f8bd1e90794741a485abd119
lopa10ko <lopa10ko239@mail.ru>
2022-11-23 19:05:50 +0300

    feat: add BankInfo meta-entity

---
+++ Lab4/Banks/Models/BankInfo.cs
@@ -0,0 +1,25 @@
+﻿using Banks.Entities;
+
+namespace Banks.Models;
+
+public class BankInfo
+{
+    public BankInfo(int commissionPeriod, long freezeTime, decimal debitPercent, decimal untrustedUserWithdrawLimit, decimal creditLimit, decimal creditCommission, DepositRules depositRules)
+    {
+        CommissionPeriod = commissionPeriod;
+        FreezeTime = freezeTime;
+        DebitPercent = debitPercent;
+        CreditLimit = creditLimit;
+        UntrustedUserWithdrawLimit = untrustedUserWithdrawLimit;
+        CreditCommission = creditCommission;
+        DepositRules = depositRules;
+    }
+
+    public int CommissionPeriod { get; }
+    public long FreezeTime { get; }
+    public decimal DebitPercent { get; }
+    public decimal CreditLimit { get; }
+    public decimal CreditCommission { get; }
+    public decimal UntrustedUserWithdrawLimit { get; }
+    public DepositRules DepositRules { get; }
+}
