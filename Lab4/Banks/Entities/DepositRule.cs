9fc8725f7ca45ee93f85be491f5f41b6f443550b
lopa10ko <lopa10ko239@mail.ru>
2022-11-23 19:04:47 +0300

    feat: add DepositRule

---
+++ Lab4/Banks/Entities/DepositRule.cs
@@ -0,0 +1,26 @@
+﻿namespace Banks.Entities;
+
+public record DepositRule
+{
+    public DepositRule(decimal criticalValue, decimal percent)
+    {
+        CriticalValue = ValidateValue(criticalValue);
+        Percent = ValidateValue(percent);
+    }
+
+    public decimal CriticalValue { get; }
+    public decimal Percent { get; }
+
+    public bool IsOverlapping(DepositRule otherRule)
+        => CriticalValue.Equals(otherRule.CriticalValue);
+
+    public DepositRule GetMaximumPercent(DepositRule otherRule)
+        => Percent > otherRule.Percent ? this : otherRule;
+
+    private static decimal ValidateValue(decimal value)
+    {
+        if (value <= 0)
+            throw new Exception();
+        return value;
+    }
+}
