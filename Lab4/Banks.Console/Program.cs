f047edace21a5228c57cd2aa0a98ea11244bc66f
lopa10ko <lopa10ko239@mail.ru>
2022-11-25 17:35:58 +0300

    feat: add basic spectre console init

--- Lab4/Banks.Console/Program.cs
+++ Lab4/Banks.Console/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+namespace Banks.Console;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var fruit = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("What's your [green]god damn choice[/]?")
+                .PageSize(5)
+                .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
+                .AddChoices(new[]
+                {
+                    "Apple", "Apricot", "Avocado",
+                    "Banana", "Blackcurrant", "Blueberry",
+                    "Cherry", "Cloudberry", "Cocunut",
+                }));
+        AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
+    }
+}
