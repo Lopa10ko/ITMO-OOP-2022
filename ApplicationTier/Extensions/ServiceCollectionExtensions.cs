10f308f7e04069e2644754bc318a8532f71cb885
lopa10ko <lopa10ko239@mail.ru>
2022-12-23 00:12:56 +0300

    feat: add extension method to add scoped services

---
+++ ApplicationTier/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationTier.Services;
+using ApplicationTier.Services.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApplicationTier.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection collection)
+    {
+        collection.AddScoped<IEmployee, EmployeeService>();
+        collection.AddScoped<IMessage, MessageService>();
+        return collection;
+    }
+}
