e4ab253cb1ebd3366dc52dc18b27f23669d8d503
lopa10ko <lopa10ko239@mail.ru>
2022-12-23 00:01:55 +0300

    feat: add DBContext configuration and EF interface mapping

---
+++ DataAccessTier/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessTier.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessTier;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Employee> Employees { get; set; } = null!;
+    public DbSet<Message> Messages { get; set; } = null!;
+
+    // TODO: decomposition need
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(builder =>
+        {
+            builder.HasMany(x => x.LedEmployees).WithOne().HasForeignKey("BossId");
+            builder.HasMany(x => x.Messages).WithOne().HasForeignKey("EmployeeId");
+        });
+    }
+}
