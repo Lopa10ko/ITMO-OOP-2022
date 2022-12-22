5ebd4a27d22af8a92e7d3ceee5a7ebccda5f36b8
lopa10ko <lopa10ko239@mail.ru>
2022-12-23 00:12:24 +0300

    feat: add logic for getting entity from Guid alone

---
+++ ApplicationTier/Extensions/DbSetExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationTier.Extensions;
+
+public static class DbSetExtensions
+{
+    public static async Task<T> GetEntityAsync<T>(this DbSet<T> set, Guid id, CancellationToken cancellationToken)
+        where T : class
+    {
+        T? entity = await set.FindAsync(new object[] { id }, cancellationToken);
+
+        if (entity is null)
+            throw new Exception("shit.................");
+            /*throw EntityNotFoundException<T>.Create(id);*/
+
+        return entity;
+    }
+}
