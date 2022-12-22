0659d57b8b55e606530914c99797d76bbb014abc
lopa10ko <lopa10ko239@mail.ru>
2022-12-23 00:05:48 +0300

    feat: introduce DataAccess Tier poor models

---
+++ DataAccessTier/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccessTier.Models;
+
+public class Employee
+{
+    public Employee(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+        LedEmployees = new List<Employee>();
+        Messages = new List<Message>();
+    }
+
+#pragma warning disable CS8618
+    protected Employee() { }
+#pragma warning restore CS8618
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Employee> LedEmployees { get; set; }
+
+    // TODO: fix timing issue to form proper report
+    public virtual ICollection<Message> Messages { get; set; }
+}
