c63917692878a9eee529e3e4cf7d423ba217c488
lopa10ko <lopa10ko239@mail.ru>
2022-12-23 00:08:54 +0300

    feat: introduce Application Tier logic interfaces

---
+++ ApplicationTier/Services/IMessage.cs
@@ -0,0 +1,9 @@
+﻿using ApplicationTier.DTO;
+using DataAccessTier.Models;
+
+namespace ApplicationTier.Services;
+
+public interface IMessage
+{
+    Task<MessageDto> CreateMessageAsync(string text, Guid employeeId, CancellationToken cancellationToken);
+}
