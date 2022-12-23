e5e7d19a9973861b3bf98f2b3e765b0f28f99cdc
lopa10ko <lopa10ko239@mail.ru>
2022-12-23 16:33:56 +0300

    feat: implement controller for Message logic

---
+++ PresentationTier/Controllers/MessageController.cs
@@ -0,0 +1,34 @@
+﻿using ApplicationTier.DTO;
+using ApplicationTier.Services;
+using Microsoft.AspNetCore.Mvc;
+using PresentationTier.Models.Messages;
+
+namespace PresentationTier.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MessageController : ControllerBase
+{
+    private readonly IMessage _messageService;
+
+    public MessageController(IMessage messageService)
+    {
+        _messageService = messageService;
+    }
+
+    public CancellationToken CancellationToken => HttpContext.RequestAborted;
+
+    [HttpPost]
+    public async Task<ActionResult<MessageDto>> CreateAsync([FromBody] CreateMessageModel model)
+    {
+        var message = await _messageService.CreateMessageAsync(model.Text, model.EmployeeId, CancellationToken);
+        return Ok(message);
+    }
+
+    [HttpGet(Name = "GetAllMessageGuids")]
+    public async Task<IEnumerable<MessageInstance>> GetAsync(Guid employeeId)
+    {
+        var messageDtos = await _messageService.GetAllMessageGuids(employeeId, CancellationToken);
+        return messageDtos.Select(dto => new MessageInstance(dto.Id, dto.EmployeeId, dto.State, dto.HandledTime));
+    }
+}
